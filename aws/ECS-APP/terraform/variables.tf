2d8e6e880465864cbdeba8d5ffb928bc48798109
Thadeu <mtguimaraes84@gmail.com>
2024-10-10 18:46:20 -0300

    feat: update files  react-native/html_css/awax/index.html

--- aws/ECS-APP/terraform/variables.tf
+++ aws/ECS-APP/terraform/variables.tf
@@ -1,4 +1,5 @@
 variable "region" {
+<<<<<<< Updated upstream
   type = string
 }
 
@@ -82,6 +83,107 @@ variable "task_minimum" {
 variable "task_maximum" {
   type = number
 }
+=======
+  type        = string
+  description = "Região da AWS onde os recursos serão provisionados."
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Nome do cluster ECS que hospedará o serviço."
+}
+
+variable "service_name" {
+  type        = string
+  description = "Nome do serviço que será usado dentro do cluster."
+}
+
+variable "service_port" {
+  type        = number
+  description = "Porta TCP na qual o serviço aceitará tráfego."
+}
+
+variable "service_cpu" {
+  type        = number
+  description = "Quantidade de CPU reservada para o serviço, em unidades definidas pela AWS."
+}
+
+variable "service_memory" {
+  type        = number
+  description = "Quantidade de memória reservada para o serviço, em megabytes."
+}
+
+variable "service_healthcheck" {
+  type        = map(any)
+  description = "Configurações do health check para o serviço, como caminho e protocolo."
+}
+
+variable "service_launch_type" {
+  type = list(object({
+    capacity_provider = string
+    weight            = number
+  }))
+}
+
+
+variable "container_image" {}
+
+variable "service_hosts" {
+  type        = list(string)
+  description = "Lista de endereços ou nomes de host atribuídos ao serviço para balanceamento de carga ou exposição."
+}
+
+variable "service_task_count" {
+  type        = number
+  description = "Número de tarefas que o serviço deve manter em execução simultaneamente."
+}
+
+variable "ssm_vpc_id" {
+  type        = string
+  description = "ID do VPC armazenado no AWS Systems Manager (SSM) onde o serviço será implantado."
+}
+
+variable "ssm_listener" {
+  type        = string
+  description = "ARN do listener de um Application Load Balancer (ALB), armazenado no AWS SSM, que será usado pelo serviço."
+}
+
+variable "ssm_private_subnet_1" {
+  type        = string
+  description = "ID da primeira subnet privada, armazenado no AWS SSM, onde o serviço será implantado."
+}
+
+variable "ssm_private_subnet_2" {
+  type        = string
+  description = "ID da segunda subnet privada, armazenado no AWS SSM, para implantação do serviço."
+}
+
+variable "ssm_private_subnet_3" {
+  type        = string
+  description = "ID da terceira subnet privada, armazenado no AWS SSM, usada para implantação do serviço."
+}
+
+variable "ssm_alb" {
+  type        = string
+  description = ""
+}
+
+variable "environment_variables" {
+  type        = list(map(string))
+  description = "Lista de variáveis de ambiente que serão passadas às tarefas do serviço."
+}
+
+variable "capabilities" {
+  type        = list(string)
+  description = "Lista de capacidades especiais necessárias para o serviço, como 'SYS_ADMIN' para determinados privilégios de sistema."
+}
+
+variable "scale_type" {}
+
+variable "task_minimum" {}
+
+variable "task_maximum" {}
+>>>>>>> Stashed changes
 
 ### Autoscaling de CPU
 
@@ -113,11 +215,22 @@ variable "scale_in_evaluation_periods" {}
 
 variable "scale_in_cooldown" {}
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 ### Tracking CPU
 variable "scale_tracking_cpu" {}
 
 
 ### Tracking Requests
+<<<<<<< Updated upstream
 
 variable "scale_tracking_requests" {}
+=======
+variable "scale_tracking_requests" {}
+
+### Service Discovery
+
+variable "ssm_service_discovery_namespace" {}
+>>>>>>> Stashed changes
